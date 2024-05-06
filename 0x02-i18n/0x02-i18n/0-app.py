42788673c55f6f25b698cff6b990ca91b406aa17
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2024-05-06 15:35:53 +0100

    fixed errors

--- 0x02-i18n/0x02-i18n/0-app.py
+++ 0x02-i18n/0x02-i18n/0-app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import Flask, render_template
 
+"""
+Flask
+"""
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,10 +12,11 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Handles / route
+    route handler
     """
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
     app.run(port="5000", host="0.0.0.0", debug=True)
+
--- 0x02-i18n/README.md
+++ 0x02-i18n/README.md
@@ -1,4 +1,1 @@
-Learning Objectives
-Learn how to parametrize Flask templates to display different languages
-Learn how to infer the correct locale based on URL parameters, user settings or request headers
-Learn how to localize timestamps
+I8N
